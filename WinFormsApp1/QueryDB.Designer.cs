c6d178e90956e1ea51d83c5b115e994a45cd888a
Alex Krepch <ak251023@ncr.com>
2021-07-27 16:38:10 +0300

    new window

---
+++ WinFormsApp1/QueryDB.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class QueryDB
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.CardNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Balance = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ValidDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnRecharge = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CardNumber,
+            this.Balance,
+            this.ValidDate,
+            this.Status});
+            this.dataGridView1.Location = new System.Drawing.Point(34, 33);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(628, 389);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // CardNumber
+            // 
+            this.CardNumber.HeaderText = "Card Number";
+            this.CardNumber.MinimumWidth = 6;
+            this.CardNumber.Name = "CardNumber";
+            this.CardNumber.Width = 200;
+            // 
+            // Balance
+            // 
+            this.Balance.HeaderText = "Balance";
+            this.Balance.MinimumWidth = 6;
+            this.Balance.Name = "Balance";
+            this.Balance.Width = 125;
+            // 
+            // ValidDate
+            // 
+            this.ValidDate.HeaderText = "Valid Date";
+            this.ValidDate.MinimumWidth = 6;
+            this.ValidDate.Name = "ValidDate";
+            this.ValidDate.Width = 125;
+            // 
+            // Status
+            // 
+            this.Status.HeaderText = "Status";
+            this.Status.MinimumWidth = 6;
+            this.Status.Name = "Status";
+            this.Status.Width = 125;
+            // 
+            // btnRecharge
+            // 
+            this.btnRecharge.Location = new System.Drawing.Point(668, 60);
+            this.btnRecharge.Name = "btnRecharge";
+            this.btnRecharge.Size = new System.Drawing.Size(120, 29);
+            this.btnRecharge.TabIndex = 1;
+            this.btnRecharge.Text = "Recharge";
+            this.btnRecharge.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(669, 96);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Amount";
+            this.textBox1.Size = new System.Drawing.Size(119, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // QueryDB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnRecharge);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "QueryDB";
+            this.Text = "QueryDB";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CardNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Balance;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ValidDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+        private System.Windows.Forms.Button btnRecharge;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
